57d05977bb6c0390f49c64aec8c046685fae22d3
Dominykas M. <26067386+Tasty-Kiwi@users.noreply.github.com>
2025-06-20 18:15:14 +0300

    setup github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pewpewlive.github.io',
+  base: 'hybroid-docs',
   integrations: [
     starlight({
       logo: {
